ba7fde54a688c0232cf2489ffd201b3f9dd9f7f1
Jonas Heilemann <jonas-heilemann@outlook.de>
2025-01-05 16:52:50 +0100

    feat: hover and select for heatmap

--- src/heatMap.ts
+++ src/heatMap.ts
@@ -1,165 +1,255 @@
 import * as d3 from 'd3'
 import { MotionTagDataParser } from './parser/motionTagDataParser'
 
-const colorRamp = [
-	'#D56062',
-	'#CB6E67',
-	'#C27C6C',
-	'#B88B71',
-	'#AF9976',
-	'#A5A87B',
-	'#9CB680',
-	'#93C586',
-	'#99C88C',
-	'#A0CB93',
-	'#A7CE9A',
-	'#ADD1A1',
-	'#B4D4A8',
-	'#BBD7AF',
-	'#C1DAB6',
-	'#C8DDBD',
-	'#CFE0C4',
-	'#D5E3CB',
-	'#DCE6D2',
-	'#E3E9D9',
-	'#EAECE0',
-	'#fff5eb',
-].reverse()
+export interface DailyDistanceHeatMapOptions {}
 
-const gridColor = '#ebedf0'
+export class DailyDistanceHeatMap {
+	private static HEIGHT = 200
+	private static DAYS = ['So', 'Sa', 'Fr', 'Do', 'Mi', 'Di', 'Mo']
+	private static MONTHS = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
+	private static COLOR_RAMP = [
+		'#fff5eb',
+		'#EAECE0',
+		'#E3E9D9',
+		'#DCE6D2',
+		'#D5E3CB',
+		'#CFE0C4',
+		'#C8DDBD',
+		'#C1DAB6',
+		'#BBD7AF',
+		'#B4D4A8',
+		'#ADD1A1',
+		'#A7CE9A',
+		'#A0CB93',
+		'#99C88C',
+		'#93C586',
+		'#9CB680',
+		'#A5A87B',
+		'#AF9976',
+		'#B88B71',
+		'#C27C6C',
+		'#CB6E67',
+		'#D56062',
+	]
+	private static GRID_COLOR = '#ebedf0'
+	private static WEEK_NR_COLOR = '#bdbec0'
+	private static HOVER_STROKE = '#000000'
+	private static SELECT_STROKE = '#000000'
 
-const days = ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'].reverse()
-const months = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
-const weeks = Array.from({ length: 53 }, (_, index) => `${index + 1}`)
-const width = window.innerWidth - 60
-const height = 200
+	private svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, unknown>
+	private svg_g: d3.Selection<SVGGElement, unknown, HTMLElement, unknown> | null = null
+	private tooltip: d3.Selection<HTMLDivElement, unknown, HTMLElement, unknown>
+	private select_rect: d3.Selection<SVGRectElement, unknown, HTMLElement, unknown> | null = null
 
-const yScale = d3.scaleBand().range([height, 0]).domain(days)
+	private xScale: d3.ScaleBand<string> | null = null
+	private yScale: d3.ScaleBand<string> | null = null
 
-const xScale = d3
-	.scaleBand()
-	.range([0, yScale.bandwidth() * weeks.length])
-	.domain(weeks)
+	private selectedEntry: { [key: string]: any; total: number; date: Date } | null = null
 
-const xOffset = (width - xScale.bandwidth() * weeks.length) / 2
-
-const svg = d3
-	.select('#heat')
-	.append('svg')
-	.attr('width', width)
-	.attr('height', 250)
-	.append('g')
-	.attr('transform', `translate(${xOffset}, ${30})`)
-
-var tooltip = d3.select('#heat').append('div').style('opacity', '0').attr('class', 'tooltip')
-
-const yAxis = svg.append('g').call(d3.axisLeft(yScale))
-yAxis.select('.domain').remove()
-yAxis
-	.selectAll('.tick line')
-	.attr('x2', xScale.bandwidth() * weeks.length)
-	.attr('stroke-opacity', 0.5)
-	.attr('stroke-width', yScale.bandwidth() - 5)
-	.attr('stroke', gridColor)
-
-const xAxis = svg.append('g').call(
-	d3.axisTop(xScale).tickFormat((domainValue, index) => {
-		const weekNr = parseInt(domainValue)
-		if (getMonthFromWeek(weekNr - 1, 2024) != getMonthFromWeek(weekNr, 2024)) {
-			return getMonthFromWeek(weekNr, 2024)
-		}
-		return domainValue
-	})
-)
-xAxis.select('.domain').remove()
-xAxis
-	.selectAll('.tick line')
-	.attr('y2', yScale.bandwidth() * days.length)
-	.attr('stroke-opacity', 0.5)
-	.attr('stroke-width', xScale.bandwidth() - 5)
-	.attr('stroke', gridColor)
-xAxis
-	.selectAll('.tick text')
-	.filter(function () {
-		return Number.isNaN(parseInt(d3.select(this).text()))
-	})
-	.attr('font-weight', 'bolder')
-
-xAxis
-	.selectAll('.tick text')
-	.filter(function () {
-		return !Number.isNaN(parseInt(d3.select(this).text()))
-	})
-	.attr('fill', '#bdbec0')
-
-export function renderHeatMap(motionTagDataParser: MotionTagDataParser, year: number) {
-	const rawData = motionTagDataParser.getDistanceHeatMap()
-
-	const data = Object.values(rawData)
-	// const inputDomain = [0, d3.max(data, (entry) => entry.total)!]
-	const inputDomain = [0, 300 * 1000]
-
-	var myColor = d3
-		.scaleQuantize()
-		// @ts-ignore
-		.range(colorRamp)
-		.domain(inputDomain)
-
-	svg
-		.selectAll()
-		.data(data)
-		.join('rect')
-		.attr('x', (entry) => xScale(`${getWeekNumber(entry.date)}`)!)
-		.attr('y', (entry) => yScale(['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'][entry.date.getDay()])!)
-		.attr('width', () => xScale.bandwidth() - 5)
-		.attr('height', () => yScale.bandwidth() - 5)
-		.attr('transform', `translate(2.5, 2.5)`)
-		.attr('value', (entry) => entry.total)
-		.attr('fill', (entry) => myColor(entry.total))
-		.on('mouseover', function (event, entry) {
-			tooltip.style('opacity', '1')
-			d3.select(this).attr('stroke', 'black')
-		})
-		.on('mousemove', function (event, entry) {
-			const distance = entry.total > 5000 ? Math.round(entry.total / 100) / 10 + 'km' : entry.total + 'm'
-			const date = `${entry.date.getDate()}. ${months[entry.date.getMonth()]} ${entry.date.getFullYear()}`
-
-			tooltip
-				.html(`${date}: ${distance}`)
-				.style('left', d3.pointer(event)[0] + 70 + xOffset + 'px')
-				.style('top', d3.pointer(event)[1] + 'px')
-		})
-		.on('mouseleave', function () {
-			tooltip.style('opacity', '0')
-			d3.select(this).attr('stroke', 'none')
-		})
-		.on('click', () => {
-			// TODO: Show date only for that date.
-		})
-}
-
-function getWeekNumber(date: Date): number {
-	const tempDate = new Date(date.valueOf())
-
-	const dayNum = (date.getDay() + 6) % 7
-
-	tempDate.setDate(tempDate.getDate() - dayNum + 3)
-
-	const firstThursday = tempDate.valueOf()
-
-	tempDate.setMonth(0, 1)
-
-	if (tempDate.getDay() !== 4) {
-		tempDate.setMonth(0, 1 + ((4 - tempDate.getDay() + 7) % 7))
+	constructor(element: string, options?: DailyDistanceHeatMapOptions) {
+		this.svg = d3.select(element).append('svg')
+		this.tooltip = d3.select('#heat').append('div').attr('class', 'tooltip').text('')
 	}
 
-	return 1 + Math.ceil((firstThursday - tempDate.valueOf()) / 604800000)
-}
+	public drawGrid(year: number) {
+		const weeks = Array.from({ length: this.getISOWeeksInYear(year) }, (_, index) => `${index + 1}`)
 
-function getMonthFromWeek(week: number, year: number) {
-	let d = new Date(year, 0, 1 + (week - 1) * 7)
-	d.getUTCDay() < 5
-		? d.setUTCDate(d.getUTCDate() - d.getUTCDay() + 1)
-		: d.setUTCDate(d.getUTCDate() + 8 - d.getUTCDay())
-	return ('' + d).split(' ')[1]
+		this.yScale = d3
+			.scaleBand()
+			.range([DailyDistanceHeatMap.HEIGHT - 16, 0])
+			.domain(DailyDistanceHeatMap.DAYS)
+
+		this.xScale = d3
+			.scaleBand()
+			.range([0, this.yScale.bandwidth() * weeks.length])
+			.domain(weeks)
+
+		const width = (this.xScale.bandwidth() + 1) * weeks.length - 23
+		console.log(this.xScale.bandwidth())
+		this.svg_g = this.svg
+			.attr('width', Math.floor(width))
+			.attr('height', DailyDistanceHeatMap.HEIGHT)
+			.append('g')
+			.attr('transform', 'translate(23, 16)')
+
+		this.tooltip.style('width', `${width - 23}px`)
+		this.tooltip.style('margin-left', `23px`)
+
+		this.select_rect = this.svg_g
+			.append('rect')
+			.style('opacity', 0)
+			.attr('width', this.xScale.bandwidth())
+			.attr('height', this.yScale.bandwidth())
+
+		this.drawAxis()
+	}
+
+	private drawAxis() {
+		if (this.yScale == null || this.xScale == null || this.svg_g == null) return
+
+		const yAxis = this.svg_g.append('g').call(d3.axisLeft(this.yScale))
+		yAxis.select('.domain').remove()
+		yAxis
+			.selectAll('.tick line')
+			.attr('x2', this.xScale.bandwidth() * this.xScale.domain().length)
+			.attr('stroke-opacity', 0.5)
+			.attr('stroke-width', this.yScale.bandwidth() - 5)
+			.attr('stroke', DailyDistanceHeatMap.GRID_COLOR)
+
+		const xAxis = this.svg_g.append('g').call(
+			d3.axisTop(this.xScale).tickFormat((domainValue) => {
+				const weekNr = parseInt(domainValue)
+				if (this.getMonthFromWeek(weekNr - 1, 2024) != this.getMonthFromWeek(weekNr, 2024)) {
+					return this.getMonthFromWeek(weekNr, 2024)
+				}
+				return domainValue
+			})
+		)
+		xAxis.select('.domain').remove()
+		xAxis
+			.selectAll('.tick line')
+			.attr('y2', this.yScale.bandwidth() * this.yScale.domain().length)
+			.attr('stroke-opacity', 0.5)
+			.attr('stroke-width', this.xScale.bandwidth() - 5)
+			.attr('stroke', DailyDistanceHeatMap.GRID_COLOR)
+		xAxis
+			.selectAll('.tick text')
+			.filter(function () {
+				return Number.isNaN(parseInt(d3.select(this).text()))
+			})
+			.attr('font-weight', 'bolder')
+
+		xAxis
+			.selectAll('.tick text')
+			.filter(function () {
+				return !Number.isNaN(parseInt(d3.select(this).text()))
+			})
+			.attr('fill', DailyDistanceHeatMap.WEEK_NR_COLOR)
+	}
+
+	private drawSelected() {
+		if (this.select_rect == null) return
+
+		this.select_rect.transition()
+		if (this.selectedEntry == null) {
+			this.select_rect.style('opacity', 0)
+			this.tooltip.html("")
+			return
+		}
+
+		this.select_rect
+			.style('opacity', 1)
+			.attr('x', (entry) => this.xScale!(`${this.getISOWeekNumber(this.selectedEntry!.date!)}`)!)
+			.attr(
+				'y',
+				(entry) => this.yScale!(['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'][this.selectedEntry!.date!.getDay()])!
+			)
+
+		this.tooltip.html(DailyDistanceHeatMap.getDescription(this.selectedEntry))
+	}
+
+	private static getDescription(entry: { [key: string]: any; total: number; date: Date }) {
+		const distance = entry.total > 5000 ? Math.round(entry.total / 100) / 10 + 'km' : entry.total + 'm'
+		const date = `${entry.date.getDate()}. ${
+			DailyDistanceHeatMap.MONTHS[entry.date.getMonth()]
+		} ${entry.date.getFullYear()}`
+
+		const modes = Object.keys(entry)
+			.filter((key) => !(key == 'date' || key == 'total'))
+			.sort((a, b) => {
+				return entry[a] < entry[b] ? 1 : -1
+			})
+			.map((key) => {
+				return `<span style="background-color: ${MotionTagDataParser.getColorForMode(key)}">${
+					entry[key] > 5000 ? Math.round(entry[key] / 100) / 10 + 'km' : entry[key] + 'm'
+				} ${key}</span>`
+			})
+			.join('\t')
+			return `<b>${date}:</b> ${distance}, <b>Modes:</b> ${modes}`
+	}
+
+
+	public drawData(data: { total: number; date: Date; [key: string]: any }[], upperBound?: number) {
+		if (this.xScale == null || this.yScale == null || this.svg_g == null) return
+
+		this.svg_g.select('g[role=data]').remove()
+		const tooltip = this.tooltip
+
+		const getSelectedDate = () => this.selectedEntry?.date || null
+		const setSelectedDate = (entry: { [key: string]: any; total: number; date: Date } | null) => {
+			this.selectedEntry = entry
+			this.drawSelected()
+		}
+
+		const colorRamp = d3
+			.scaleQuantize()
+			// @ts-expect-error
+			.range(DailyDistanceHeatMap.COLOR_RAMP)
+			.domain([0, upperBound ?? 300 * 1000])
+
+		this.svg_g
+			.append('g')
+			.attr('role', 'data')
+			.selectAll()
+			.data(data)
+			.join('rect')
+			.attr('x', (entry) => this.xScale!(`${this.getISOWeekNumber(entry.date)}`)!)
+			.attr('y', (entry) => this.yScale!(['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'][entry.date.getDay()])!)
+			.attr('width', () => this.xScale!.bandwidth() - 5)
+			.attr('height', () => this.yScale!.bandwidth() - 5)
+			.attr('transform', `translate(2.5, 2.5)`)
+			// .attr('border-radius', "")
+			.attr('fill', (entry) => colorRamp(entry.total))
+			.on('mouseover', function (event, entry) {
+				d3.select(this).attr('stroke', 'black')
+				if (getSelectedDate() == null) tooltip.html(DailyDistanceHeatMap.getDescription(entry))
+			})
+			.on('mouseleave', function (event, entry) {
+				d3.select(this).attr('stroke', 'none')
+				if (getSelectedDate() == null) tooltip.html("")
+			})
+			.on('click', function (event, entry) {
+				if (getSelectedDate() != null && getSelectedDate()!.toDateString() == entry.date.toDateString()) {
+					setSelectedDate(null)
+					return
+				}
+				setSelectedDate(entry)
+			})
+	}
+
+	// -- Date functions
+
+	private getISOWeeksInYear(year: number): number {
+		const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+
+		const lastDayOfYear = new Date(year, 11, 31)
+
+		return lastDayOfYear.getDay() == 4 || (isLeapYear && lastDayOfYear.getDay() == 5) ? 53 : 52
+	}
+
+	private getISOWeekNumber(date: Date): number {
+		const tempDate = new Date(date.valueOf())
+
+		const dayNum = (date.getDay() + 6) % 7
+
+		tempDate.setDate(tempDate.getDate() - dayNum + 3)
+
+		const firstThursday = tempDate.valueOf()
+
+		tempDate.setMonth(0, 1)
+
+		if (tempDate.getDay() !== 4) {
+			tempDate.setMonth(0, 1 + ((4 - tempDate.getDay() + 7) % 7))
+		}
+
+		return 1 + Math.ceil((firstThursday - tempDate.valueOf()) / 604800000)
+	}
+
+	private getMonthFromWeek(week: number, year: number) {
+		let d = new Date(year, 0, 1 + (week - 1) * 7)
+		d.getUTCDay() < 5
+			? d.setUTCDate(d.getUTCDate() - d.getUTCDay() + 1)
+			: d.setUTCDate(d.getUTCDate() + 8 - d.getUTCDay())
+		return DailyDistanceHeatMap.MONTHS[d.getMonth()]
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Control, LatLng, LayerGroup, Map, Polyline, TileLayer } from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import { VisualizeControl } from './controls/VisualizeControl'
-import { renderHeatMap } from './heatMap'
+import { DailyDistanceHeatMap } from './heatMap'
 import './index.css'
 import { MotionTagDataParser } from './parser/motionTagDataParser'
 
 const parser = new MotionTagDataParser()
+const dailyDistance = new DailyDistanceHeatMap("#heat")
+
+dailyDistance.drawGrid(2024);
 
 const map: Map = new Map('map', {
 	zoom: 7,
@@ -32,7 +35,8 @@ export async function setData(d: string) {
 	await parser.parse(d)
 
 	console.log('Parsing data took', Date.now() - start + 'ms')
-	renderHeatMap(parser, 0)
+	
+	dailyDistance.drawData(Object.values(parser.getDistanceHeatMap()))
 
 	// parser.getStays().forEach((stay) => {
 	// 	new Marker(stay.position)
@@ -45,43 +49,10 @@ export async function setData(d: string) {
 			layerControl.addOverlay(layerGroups[movement.mode], movement.mode)
 		}
 		new Polyline(movement.path, {
-			color: getColorForMode(movement.mode),
+			color: MotionTagDataParser.getColorForMode(movement.mode),
 			weight: 5,
 		}).addTo(layerGroups[movement.mode])
 	}
 }
 
-function getColorForMode(mode: string) {
-	// Not every Mode is currently mapped
-	switch (mode) {
-		case 'Hiking':
-		case 'Walk':
-			return '#dfba06'
-		case 'LightRail':
-			return '#37ab9e'
-		case 'RegionalTrain':
-			return '#da73a0'
-		case 'Train':
-			return '#cd3841'
-		case 'Ecar':
-		case 'Car':
-			return '#d48004'
-		case 'Subway':
-			return '#3d8eb4'
-		case 'Tram':
-			return '#2dabc4'
-		case 'Bicycle':
-			return '#8da433'
-		case 'Airplane':
-			return '#e8b100'
-		case 'Bus':
-			return '#5178ab'
-		case 'Boat':
-			return '#6d6db0'
-		case 'Cablecar':
-		case 'KickScooter':
-		default:
-			console.log('No color for:', mode)
-			return '#4c8b9c'
-	}
-}
+
