7c59373a82b76c7b19c10e4c7a1129229f4625cd
Jonas Heilemann <jonas-heilemann@outlook.de>
2025-01-05 13:28:25 +0100

    fix: asset path for build

--- esbuild.config.js
+++ esbuild.config.js
@@ -24,7 +24,7 @@ if (process.argv[2] == '--serve') {
 	const context = await esbuild.context(
 		createBuildSettings({
 			sourcemap: true,
-			publicPath: '/build/',
+			publicPath: './build/',
 			banner: {
 				js: `new EventSource('/esbuild').addEventListener('change', () => location.reload());`,
 			},
@@ -41,7 +41,7 @@ if (process.argv[2] == '--serve') {
 
 	console.log(`Serving app at ${host}:${port}.`)
 } else if (process.argv[2] == '--build') {
-	await esbuild.build(createBuildSettings({ minify: true, publicPath: '/build/' }))
+	await esbuild.build(createBuildSettings({ minify: true, publicPath: './build/' }))
 } else {
 	console.log('Specify operation mode: --build or --serve')
 }
